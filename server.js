98170d120bbaf23caaba03d9b33da9473fff9703
Thawat-2061 <65011212061@msu.ac.th>
2025-12-22 18:38:18 +0700

    test004

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
-import admin from "firebase-admin";
-import dotenv from "dotenv";
-dotenv.config();
+
+
+import { bucket, db } from "./firebaseAdmin.js";
 
 const app = express();
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// Firebase Admin SDK initialization
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  }),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
-
-
-const db = admin.firestore();
-export const bucket = admin.storage().bucket();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* ---------- ROUTES ---------- */
 // Registration route
